40ae3f99d387cfc0a83230f294188b2c505b23b2
aruk323 <kakesyari@gmail.com>
2026-07-02 11:32:27 +0900

    Expand chapter 3 to full playable route

--- data/chapters/chapterIndex.js
+++ data/chapters/chapterIndex.js
@@ -1,5 +1,6 @@
 window.NOVEL_CHAPTER_INDEX = [
   { id: "prologue", file: "data/chapters/prologue.js" },
   { id: "chapter01", file: "data/chapters/chapter01.js" },
-  { id: "chapter02", file: "data/chapters/chapter02.js" }
+  { id: "chapter02", file: "data/chapters/chapter02.js" },
+  { id: "chapter03", file: "data/chapters/chapter03.js" }
 ];
